8f8e6bceab7bd2b5b6cb76dbba1c20b1c8561fce
Arthur Scapin Pereira <106567677+ArtScapin@users.noreply.github.com>
2022-07-11 23:12:07 -0300

    Update main.c

--- main.c
+++ main.c
@@ -11,10 +11,21 @@ int main()
     srand(time(NULL));
     system("clear");
     
-    supertrunfo Jogador1[24], Jogador2[24];
+    supertrunfo Jogador1[24], Jogador2[24], mesa[24];
+    int proximo, habilidade, ganhou, fim=0;
     
     inserir_baralho();
     embaralhar(10);
     distribuir(Jogador1, Jogador2);
+    proximo = sorteio_inicial();
+    do{
+        habilidade = escolher_habilidade(proximo, Jogador1, Jogador2);
+        ganhou = ganhador(Jogador1, Jogador2, habilidade);
+        tela(Jogador1, Jogador2, habilidade, proximo, ganhou);
+        sistema(Jogador1, Jogador2, mesa);
+        getchar();
+        system("clear");
+    }while(fim!=1);
+    
     return 0;
 }
