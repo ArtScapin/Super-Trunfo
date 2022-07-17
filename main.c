e7cc3f3a47734ece70f9daae88d071349cef3032
Arthur Scapin Pereira <106567677+ArtScapin@users.noreply.github.com>
2022-07-17 00:27:35 -0300

    Update main.c

--- main.c
+++ main.c
@@ -22,18 +22,24 @@ int main()
     embaralhar(10);
     distribuir(Jogador1, Jogador2);
     proximo = sorteio_inicial();
+    printf("\n\n\n\n");
     do{
+        printf("\n\nPressione Enter para continuar...");
+        getchar();
+        system("clear");
         habilidade = escolher_habilidade(proximo, Jogador1, Jogador2);
         ganhou = ganhador(Jogador1, Jogador2, habilidade);
-        tela(Jogador1, Jogador2, habilidade, proximo, ganhou);
-        sistema(Jogador1, Jogador2, mesa, ganhou);
+        tela(Jogador1, Jogador2, mesa, habilidade, proximo, ganhou);
         if(ganhou==1 || ganhou==2)
             proximo=ganhou;
         else
             proximo=ganhou-proximo;
-        getchar();
-        system("clear");
     }while(fim_jogador1!=24 && fim_jogador2!=24);
+    system("clear");
+    if(fim_jogador1==24)
+        printf("Parabens Jogador 1, voce foi o grande VENCEDOR!!!!");
+    else
+        printf("Parabens Jogador 2, voce foi o grande VENCEDOR!!!!");
     
     return 0;
 }
